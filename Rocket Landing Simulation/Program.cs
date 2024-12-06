0c30de07dde8d854008247153e393bc37571dd8e
HiCapa9 <hicapa91@gmail.com>
2024-12-06 11:56:08 +0200

    First commit

---
+++ Rocket Landing Simulation/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Rocket_Landing_Simulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rocket = "       /^\\\r\n      |   |\r\n      |   |\r\n     /|:|:|\\\r\n    /||::||\\\r\n   /::|::|::\\\r\n  /:::|::|:::\\  \r\n /::::|::|::::\\  \r\n/::::/   \\::::\\\r\n|____|     |____|\r\n";
+
+            for (int i = 0; i < 10; i++) { 
+                Console.Clear();
+                for (int j = 0; j < i; j++) {
+                    Console.WriteLine("\r\n");
+                }
+                Console.WriteLine(rocket);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("The rocket Landed");
+        }
+    }
+}
